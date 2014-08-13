500ba7ef53f17ea1bf08703770fe443228d8ea33
mkryuk <mkryuk@gmail.com>
2014-08-13 17:43:07 +0300

    Directive to Directive Communication

--- script/main.js
+++ script/main.js
@@ -1,131 +1,53 @@
 var myApp = angular.module("myApp",[]);
 
-//angular service
-myApp.factory('Data',function(){
-    return {message:"i'm data from a service"};
-});
+myApp.directive("superhero",function(){
+    return{
+        restrict:"E",
+        scope:{},
+        controller:function($scope){
+            $scope.abilities = [];
 
-
-myApp.factory('Avengers',function(){
-    var Avengers = {};
-    Avengers.cast = [
-        {
-            name: "Robert Downey Jr.",
-            character: "Tony Stark / Iron Man"
+            this.addStrength = function(){
+                $scope.abilities.push("strength");
+            };
+            this.addSpeed = function(){
+                $scope.abilities.push("speed");
+            };
+            this.addFlight = function(){
+                $scope.abilities.push("flight");
+            };
         },
-        {
-            name: "Chris Evans",
-            character: "Steve Rogers / Captain America"
-        },
-        {
-            name: "Mark Ruffalo",
-            character: "Bruce Banner / The Hulk"
-        },
-        {
-            name: "Chris Hemsworth",
-            character: "Thor"
-        },
-        {
-            name: "Scarlett Johansson",
-            character: "Natasha Romanoff / Black Widow"
-        },
-        {
-            name: "Jeremy Renner",
-            character: "Clint Barton / Hawkeye"
-        },
-        {
-            name: "Tom Hiddleston",
-            character: "Loki"
-        },
-        {
-            name: "Clark Gregg",
-            character: "Agent Phil Coulson"
-        },
-        {
-            name: "Cobie Smulders",
-            character: "Agent Maria Hill"
-        },
-        {
-            name: "Stellan Skarsgard",
-            character: "Selvig"
-        },
-        {
-            name: "Samuel L. Jackson",
-            character: "Nick Fury"
-        },
-        {
-            name: "Gwyneth Paltrow",
-            character: "Pepper Potts"
-        },
-        {
-            name: "Paul Bettany",
-            character: "Jarvis (voice)"
-        },
-        {
-            name: "Alexis Denisof",
-            character: "The Other"
-        },
-        {
-            name: "Tina Benko",
-            character: "NASA Scientist"
+        link: function(scope, element){
+            element.bind("mouseenter", function(){
+                console.log(scope.abilities);
+            });
         }
-    ];
-    return Avengers;
-});
-
-
-//filters
-myApp.filter('reverse', function(){
-    return function(text){
-        return text.split("").reverse().join("");
-    }
-});
-
-myApp.filter('search',function(Avengers){
-    return function(text, Avengers){
-        return Avengers.cast.search()
-    }
-});
-
-myApp.controller('FirstCtrl',['$scope','Data', function($scope, Data){
-    $scope.data = Data;
-}]);
-
-function SecondCtrl($scope, Data){
-    $scope.data = Data;
-    $scope.reversedMessage = function(message){
-        return message.split("").reverse().join("");
     };
-}
+})
 
-//Controller
-//function FirstCtrl($scope, Data){
-//    $scope.data = Data;
-//}
-
-function AvengersCtrl($scope,Avengers){
-    $scope.avangers = Avengers;
-}
-
-myApp.directive("superman",function(){
+myApp.directive("strength",function(){
     return {
-        restrict: "E",
-        template: "<div>Here I am to save the day</div>"
+        require:"superhero",
+        link:function(scope,element,attrs,superheroCtrl){
+            superheroCtrl.addStrength();
+        }
     }
-});
+})
 
-myApp.directive('enter',function(){
-    return function(scope,element,attrs){
-        element.bind('mouseenter',function(){
-            element.addClass(attrs.enter);
-        });
+myApp.directive("speed",function(){
+    return {
+        require:"superhero",
+        link:function(scope,element,attrs,superheroCtrl){
+            superheroCtrl.addSpeed();
+        }
     }
-});
+})
 
-myApp.directive('leave',function(){
-    return function(scope,element,attrs){
-        element.bind('mouseleave',function(){
-            element.removeClass(attrs.enter);
-        });
+myApp.directive("flight",function(){
+    return {
+        require:"superhero",
+        link:function(scope,element,attrs,superheroCtrl){
+            superheroCtrl.addFlight();
+        }
     }
-});
+})
