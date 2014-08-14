33de77273538ca90ff22df384180f75697e35d75
mkryuk <mkryuk@gmail.com>
2014-08-14 16:39:20 +0300

    Isolate Scope "@"

--- script/main.js
+++ script/main.js
@@ -1,53 +1,15 @@
 var myApp = angular.module("myApp",[]);
 
-myApp.directive("superhero",function(){
-    return{
-        restrict:"E",
-        scope:{},
-        controller:function($scope){
-            $scope.abilities = [];
 
-            this.addStrength = function(){
-                $scope.abilities.push("strength");
-            };
-            this.addSpeed = function(){
-                $scope.abilities.push("speed");
-            };
-            this.addFlight = function(){
-                $scope.abilities.push("flight");
-            };
+myApp.controller("appCtrl",function($scope){
+    $scope.ctrlrFlavor = "blackberry";
+});
+
+myApp.directive("drink",function(){
+    return {
+        scope:{
+            flavor:"@"
         },
-        link: function(scope, element){
-            element.bind("mouseenter", function(){
-                console.log(scope.abilities);
-            });
-        }
+        templateUrl:"tmplt.html"
     };
-})
-
-myApp.directive("strength",function(){
-    return {
-        require:"superhero",
-        link:function(scope,element,attrs,superheroCtrl){
-            superheroCtrl.addStrength();
-        }
-    }
-})
-
-myApp.directive("speed",function(){
-    return {
-        require:"superhero",
-        link:function(scope,element,attrs,superheroCtrl){
-            superheroCtrl.addSpeed();
-        }
-    }
-})
-
-myApp.directive("flight",function(){
-    return {
-        require:"superhero",
-        link:function(scope,element,attrs,superheroCtrl){
-            superheroCtrl.addFlight();
-        }
-    }
-})
+});
