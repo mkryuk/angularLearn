2d83a092c7a71f72bf5d44113ac3d9541f9834dd
mkryuk <mkryuk@gmail.com>
2014-08-21 17:30:42 +0300

    Resolve

--- script/main.js
+++ script/main.js
@@ -5,24 +5,20 @@ app.config(function($routeProvider){
     $routeProvider
         .when("/",{
             templateUrl:"app.html",
-            controller:"AppCtrl"
+            controller:"AppCtrl",
+            resolve:{
+                app:function($q, $timeout){
+                    var defer = $q.defer();
+                    $timeout(function(){
+                        defer.resolve();
+                    }, 2000);
+                    return defer.promise;
+                }
+            }
         })
 });
 
 app.controller("AppCtrl", function ($scope, $q) {
-    var defer = $q.defer();
-
-    defer.promise
-        .then(function(weapon){
-            alert("You can take my "+ weapon);
-            return "bow";
-        }).then(function(weapon){
-            alert("And my "+ weapon);
-            return "axe";
-        }).then(function(weapon){
-            alert("And my "+ weapon);
-        });
-    defer.resolve("sword");
     $scope.model = {
         message: "This is my app"
     }
