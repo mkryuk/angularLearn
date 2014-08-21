da64a1cd4543d3f86d8f8759c4f409a641b69582
mkryuk <mkryuk@gmail.com>
2014-08-21 17:00:18 +0300

    redirectTo

--- script/main.js
+++ script/main.js
@@ -1,12 +1,22 @@
 var app = angular.module("app", ["ngRoute"]);
 
+
 app.config(function($routeProvider){
     $routeProvider
-        .when('/map/:country/:state/:city',{
-            templateUrl:"app.html",
-            controller:"AppCtrl"
+        .when("/pizza/:crust/:toppings",{
+        redirectTo:function(routeParams, path, search){
+            console.log(routeParams);
+            console.log(path);
+            console.log(search);
+            return '/'+routeParams.crust;
+        }
+    })
+        .when("/deep", {
+            template:"deep dish"
         })
-
+        .otherwise({
+            redirectTo: "/"
+        })
 });
 
 app.controller("AppCtrl", function ($scope, $routeParams) {
