8eececa7f40143ea058a3833a387c614d17419af
mkryuk <mkryuk@gmail.com>
2014-08-21 16:00:30 +0300

    $routeProvider api

--- script/main.js
+++ script/main.js
@@ -1,16 +1,23 @@
-var app = angular.module("app",["ngRoute"]);
+var app = angular.module("app", ["ngRoute"]);
 
-app.config(function($routeProvider){
-    $routeProvider.when('/',
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/',
         {
-            templateUrl:'app.html',
-            controller:"AppCtrl"
-        }
-    )
+            templateUrl: 'app.html',
+            controller: "AppCtrl"
+        })
+        .when('/someText',
+        {
+            template:"some text "
+        })
+        .otherwise({
+            template:"404 NOT FOUND"
+        })
 });
 
-app.controller("AppCtrl", function($scope){
+app.controller("AppCtrl", function ($scope) {
     $scope.model = {
-        message:"This is my app!!!"
+        message: "This is my app!!!"
     }
 });
