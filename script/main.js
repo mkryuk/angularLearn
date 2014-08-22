cb1105a4c80b1680c446acfed2dcdad200feea02
mkryuk <mkryuk@gmail.com>
2014-08-22 12:48:16 +0300

    Providers

--- script/main.js
+++ script/main.js
@@ -1,45 +1,23 @@
-var app = angular.module("app", ["ngRoute"]);
+var app = angular.module("app", []);
 
-app.config(function($routeProvider, $locationProvider){
-    $routeProvider
-        .when("/", {
-            templateUrl:"app.html",
-            controller:"ViewCtrl"
-        })
-        .when("/new",{
-            templateUrl:"new.html",
-            controller:"NewCtrl",
-            resolve:{
-                loadData:viewCtrl.loadData
-            }
-        })
-});
-
-app.controller("AppCtrl",function($scope,$rootScope, $route, $location){
-    $rootScope.$on("$routeChangeStart", function(event, current, previous, resolve){
-        console.log($scope,$rootScope, $route, $location);
-    });
-
-    $rootScope.$on("$routeChangeSuccess", function(event, current, previous, resolve){
-        console.log($scope,$rootScope, $route, $location);
-    });
-});
-
-var viewCtrl = app.controller("ViewCtrl", function($scope, $route, $location){
-    $scope.changeRoute = function(){
-        console.log($scope);
-        $location.path("/new");
+app.provider("game", function(){
+    var type;
+    return{
+        setType:function(value)
+        {
+            type = value;
+        },
+        $get:function(){
+            return{
+                title:type+" Craft 2"}
+        }
     };
 });
 
-app.controller("NewCtrl", function($scope, loadData, $template){
-    console.log($scope, loadData, $template);
+app.config(function(gameProvider){
+    gameProvider.setType("War");
 });
 
-viewCtrl.loadData = function($q, $timeout){
-    var defer = $q.defer();
-    $timeout(function(){
-        defer.resolve({message:"success"});
-    }, 2000);
-    return defer.promise;
-};
+app.controller("AppCtrl", function($scope, game){
+    $scope.title = game.title;
+});
