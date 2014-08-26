07cd4e3f00ee52c5ea75140f0badd83e15e56995
mkryuk <mkryuk@gmail.com>
2014-08-26 16:30:39 +0300

    animation basics

--- script/main.js
+++ script/main.js
@@ -1,38 +1,5 @@
-var app = angular.module("app", []);
+var app = angular.module("app", ["ngAnimate"]);
 
-app.directive("country",function(){
-   return{
-       restrict:"E",
-       controller:function(){
-           this.makeAnnouncement = function(message){
-               console.log("Country says: "+message);
-           }
-       }
-   };
-});
-
-app.directive("state",function(){
-    return{
-        restrict:"E",
-        require:"^country",
-        link:function(scope, element, attrs, countryCtrl){
-
-        },
-        controller:function(){
-            this.makeLaw = function(law){
-                console.log("Law: "+law);
-            }
-        }
-    };
-});
-
-app.directive("city", function(){
-    return{
-        restrict:"E",
-        require:["^country","^state"],
-        link:function(scope,element,attrs,ctrlrs){
-            ctrlrs[0].makeAnnouncement("from city");
-            ctrlrs[1].makeLaw("Jump higher");
-        }
-    };
+app.controller("AppCtrl",function(){
+    this.toggle = true;
 });
