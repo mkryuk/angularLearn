44878daa7c6f0533836a1531f64a123ff14406c5
mkryuk <mkryuk@gmail.com>
2014-08-26 15:43:06 +0300

    Experimental "Controller as" Syntax

--- script/main.js
+++ script/main.js
@@ -1,16 +1,7 @@
 var app = angular.module("app", []);
 
-app.config(function($logProvider){
-    //if we change it to false log will be disabled
-    $logProvider.debugEnabled(true);
-});
-
-app.run(function($rootScope, $log){
-    $rootScope.$log = $log;
-});
-
-app.controller("appCtrl", function($scope){
-    $scope.myFunc = function(ev){
-
-    }
+app.controller("roomCtrl", function(){
+    this.openDoor = function(){
+        alert("creak");
+    };
 });
