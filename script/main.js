a672de6c2418d5dd7160a015a9ef951358aa6cac
mkryuk <mkryuk@gmail.com>
2014-08-26 16:01:23 +0300

    Directive Communication

--- script/main.js
+++ script/main.js
@@ -1,7 +1,38 @@
 var app = angular.module("app", []);
 
-app.controller("roomCtrl", function(){
-    this.openDoor = function(){
-        alert("creak");
+app.directive("country",function(){
+   return{
+       restrict:"E",
+       controller:function(){
+           this.makeAnnouncement = function(message){
+               console.log("Country says: "+message);
+           }
+       }
+   };
+});
+
+app.directive("state",function(){
+    return{
+        restrict:"E",
+        require:"^country",
+        link:function(scope, element, attrs, countryCtrl){
+
+        },
+        controller:function(){
+            this.makeLaw = function(law){
+                console.log("Law: "+law);
+            }
+        }
+    };
+});
+
+app.directive("city", function(){
+    return{
+        restrict:"E",
+        require:["^country","^state"],
+        link:function(scope,element,attrs,ctrlrs){
+            ctrlrs[0].makeAnnouncement("from city");
+            ctrlrs[1].makeLaw("Jump higher");
+        }
     };
 });
