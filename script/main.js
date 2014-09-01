b655bc752ff8152b10ecefef66e7c90810ee8f48
mkryuk <mkryuk@gmail.com>
2014-09-01 19:57:52 +0300

    Introduction to ui-router 1

--- script/main.js
+++ script/main.js
@@ -1,5 +1,41 @@
-var app = angular.module("app", ["ngAnimate"]);
+var app = angular.module("app", ["ui.router"]);
 
-app.controller("AppCtrl",function(){
-    this.toggle = true;
+app.config(function($stateProvider, $urlRouterProvider){
+    $stateProvider
+        .state('home', {
+            url:'/home',
+            templateUrl:'list.html'
+        })
+        .state('list',{
+            url:'/list',
+            templateUrl:'list.html',
+            controller:'ListCtrl'
+        })
+        .state('list.item',{
+        url:'/:item',
+            templateUrl:'list.item.html',
+            controller:function($scope, $stateParams){
+                $scope.item = $stateParams.item;
+            }
+
+    });
+});
+
+app.controller('ListCtrl',function($scope){
+    $scope.shoppingList = [
+        {name:'Milk'},
+        {name: 'Eggs'},
+        {name: 'Bread'},
+        {name: 'Cheese'},
+        {name: 'Ham'}
+    ];
+
+    $scope.selectItem = function(selectedItem){
+        _($scope.shoppingList).each(function(item){
+            item.selected = false;
+            if(selectedItem === item){
+                selectedItem.selected = true;
+            }
+        });
+    };
 });
