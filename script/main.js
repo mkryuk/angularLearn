6e4724648e2b552ae2d58a206d720468fcdd5842
maxim <mkryuk@gmail.com>
2014-09-07 18:44:53 +0300

    No Custom Markup

--- script/main.js
+++ script/main.js
@@ -1,8 +1,14 @@
 var app = angular.module("app", []);
 
-app.run(function($rootScope){
-    $rootScope.random = Math.random();
+app.controller("AppCtrl", function(){
+    this.random = Math.random();
+});
+
+app.directive("app",function(){
+    return{
+        restrict:"C",
+        controller:"AppCtrl as app"
+    };
 });
 
 angular.bootstrap(document.getElementById("container"), ["app"]);
-angular.bootstrap(document.getElementById("container2"), ["app"]);
