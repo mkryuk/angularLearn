834e9c004e76d11b920638543f9cfdfa9eb969df
maxim <mkryuk@gmail.com>
2014-09-07 18:25:26 +0300

    Angular bootstrap an alternative way to initialize your app

--- script/main.js
+++ script/main.js
@@ -1,41 +1,8 @@
-var app = angular.module("app", ["ui.router"]);
+var app = angular.module("app", []);
 
-app.config(function($stateProvider, $urlRouterProvider){
-    $stateProvider
-        .state('home', {
-            url:'/home',
-            templateUrl:'list.html'
-        })
-        .state('list',{
-            url:'/list',
-            templateUrl:'list.html',
-            controller:'ListCtrl'
-        })
-        .state('list.item',{
-        url:'/:item',
-            templateUrl:'list.item.html',
-            controller:function($scope, $stateParams){
-                $scope.item = $stateParams.item;
-            }
-
-    });
+app.run(function($rootScope){
+    $rootScope.random = Math.random();
 });
 
-app.controller('ListCtrl',function($scope){
-    $scope.shoppingList = [
-        {name:'Milk'},
-        {name: 'Eggs'},
-        {name: 'Bread'},
-        {name: 'Cheese'},
-        {name: 'Ham'}
-    ];
-
-    $scope.selectItem = function(selectedItem){
-        _($scope.shoppingList).each(function(item){
-            item.selected = false;
-            if(selectedItem === item){
-                selectedItem.selected = true;
-            }
-        });
-    };
-});
+angular.bootstrap(document.getElementById("container"), ["app"]);
+angular.bootstrap(document.getElementById("container2"), ["app"]);
